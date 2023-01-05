e40baee9bdbf3a963fbcd2b37043084ca7290169
rwxdan <dan1elcastro@proton.me>
2023-01-05 19:25:42 -0400

    Grammar

--- src/components/WhoAmI/WhoAmI.tsx
+++ src/components/WhoAmI/WhoAmI.tsx
@@ -1,6 +1,6 @@
 import './whoami.css';
 import { motion } from 'framer-motion';
-import LangSphere from '../LangSphere/LangSphere';
+import LangSphere from '../Knowledge/Knowledge';
 const WhoAmI = () => {
   return (
     <section className="whoami">
@@ -14,14 +14,13 @@ const WhoAmI = () => {
           <h3 className="title">WhoAmI</h3>
           <p className="text">
             Hi there! My name is Daniel. I'm a self-taught Web developer from
-            Venezuela. Since I was a child I have been curious about how
+            Venezuela. Since I was a child I've been curious about how
             <span className="italic"> things</span> work, sometimes to{' '}
             <span className="bold">build</span> something new based on that
             knowledge, often times to <span className="bold">transform</span>{' '}
-            them — Yeah, during my childhood my mom was never{' '}
-            <span className="italic">too</span> happy about the latter — Truth
-            to be told,{' '}
-            <span className="bold"> I Love learning new things!</span>
+            them — To be honest, during my childhood my mom was never{' '}
+            <span className="italic">too</span> happy about the latter — Anyhow,{' '}
+            <span className="bold"> I do love learning new things!</span>
           </p>
           <p className="text">
             Before being around the <span className="mono">IT</span> world I
@@ -41,10 +40,11 @@ const WhoAmI = () => {
           </p>
           <p className="text">
             Fast-forward to today, I've had the joy of learning many things in
-            regards to <span className="bold">Software Development</span>, also{' '}
+            regards to <span className="bold">Software Development</span>,{' '}
             <span className="bold">System Administration</span> and{' '}
-            <span className="bold">Networking</span>. I'm proud to say that I've
-            also had the privilege of collaborating with engineers at{' '}
+            <span className="bold">Networking</span>. I might not be an expert
+            on any of those, but I'm proud to say that I've had the privilege of
+            collaborating with engineers at{' '}
             <a href="https://www.cisco.com/" className="external-link">
               Cisco
             </a>
@@ -56,8 +56,8 @@ const WhoAmI = () => {
             <a href="https://www.oracle.com/" className="external-link">
               Oracle
             </a>
-            . But it turns out that what I'm most passionate about is{' '}
-            <span className="bold">Front-end Web Development!</span>
+            . You wouldn't imagine, it turns out that what I'm most passionate
+            about is <span className="bold">Front-end Web Development!</span>
           </p>
         </div>
         <LangSphere />
