53e4fbfc01c504ee683f1768ed27ff5a91f9ac78
rwxdan <dan1elcastro@proton.me>
2023-01-06 03:00:51 -0400

    Grammar Also fix links targets and correct the BCV href

--- src/components/WhoAmI/WhoAmI.tsx
+++ src/components/WhoAmI/WhoAmI.tsx
@@ -13,20 +13,21 @@ const WhoAmI = () => {
           <h3 className="title">WhoAmI</h3>
           <p className="text">
             Hi there! My name is Daniel. I'm a self-taught Web developer from
-            Venezuela. Since I was a child I've been curious about how
-            <span className="italic"> things</span> work, sometimes to{' '}
-            <span className="bold">build</span> something new based on that
-            knowledge, often times to <span className="bold">transform</span>{' '}
-            them — To be honest, during my childhood my mom was never{' '}
-            <span className="italic">too</span> happy about the latter — Anyhow,{' '}
-            <span className="bold"> I do love learning new things!</span>
+            Venezuela. As far back as I can remember, I've always been curious
+            about how
+            <span className="italic"> things</span> work. Many times I found
+            myself <span className="bold">solving</span> little puzzles for fun,{' '}
+            <span className="bold">building</span> something new, and then{' '}
+            <span className="bold">transforming </span>it. Truth to be told, I
+            do love <span className="bold">learning</span> new things!
           </p>
           <p className="text">
             Before being around the <span className="mono">IT</span> world I
             studied <span className="bold">Finances</span>, I even got a job at
             the{' '}
             <a
-              href="https://en.wikipedia.org/wiki/Banco_de_Venezuela"
+              target="_blank"
+              href="https://en.wikipedia.org/wiki/Central_Bank_of_Venezuela"
               className="external-link"
             >
               BCV
@@ -38,25 +39,37 @@ const WhoAmI = () => {
             and <span className="mono">Linux</span> in mind, so why not?
           </p>
           <p className="text">
-            Fast-forward to today, I've had the joy of learning many things in
-            regards to <span className="bold">Software Development</span>,{' '}
+            Fast-forward to today, I've had the joy of gathering knowledge and
+            skills around <span className="bold">Software Development</span>,{' '}
             <span className="bold">System Administration</span> and{' '}
             <span className="bold">Networking</span>. I might not be an expert
             on any of those, but I'm proud to say that I've had the privilege of
             collaborating with engineers from{' '}
-            <a href="https://www.cisco.com/" className="external-link">
+            <a
+              target="_blank"
+              href="https://www.cisco.com/"
+              className="external-link"
+            >
               Cisco
             </a>
             ,{' '}
-            <a href="https://www.redhat.com/en" className="external-link">
+            <a
+              target="_blank"
+              href="https://www.redhat.com/en"
+              className="external-link"
+            >
               Red Hat
             </a>{' '}
             and{' '}
-            <a href="https://www.oracle.com/" className="external-link">
+            <a
+              target="_blank"
+              href="https://www.oracle.com/"
+              className="external-link"
+            >
               Oracle
             </a>
             . You wouldn't imagine, it turns out that what I'm most passionate
-            about is <span className="bold">Front-end Web Development!</span>
+            about is <span className="bold">Web Development!</span>
           </p>
         </div>
         <LangSphere />
