2fe06d3a64ba70baf542c31c0525263531efe995
rwxdan <dan1elcastro@proton.me>
2023-01-06 15:23:24 -0400

    Create icons index

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+export { default as Menu } from './menu.svg';
+export { default as Close } from './close.svg';
+export { default as Share } from './share.svg';
+export { default as Discord } from './discord.svg';
+export { default as FrontendMentor } from './frontendmentor.svg';
+export { default as GitHub } from './github.svg';
+export { default as Linkedin } from './linkedin.svg';
+export { default as Protonmail } from './protonmail.svg';
+export { default as Telegram } from './telegram.svg';
