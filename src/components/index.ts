428a765e68f30ffe59a22a9ebbb9e9be39e5678a
rwxdan <dan1elcastro@proton.me>
2023-01-06 01:11:01 -0400

    Create Projects component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Hero } from './Hero/Hero';
 export { default as AnchorLinks } from './AnchorLinks/AnchorLinks';
 export { default as AnchorMail } from './AnchorMail/AnchorMail';
 export { default as WhoAmI } from './WhoAmI/WhoAmI';
+export { default as Projects } from './Projects/Projects';
