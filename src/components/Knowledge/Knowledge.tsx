fa0e9281cd240a61bdce7289ac31925b8673b5bc
rwxdan <dan1elcastro@proton.me>
2023-01-06 18:22:32 -0400

    Recreate component using react-tag-cloud

--- src/components/Knowledge/Knowledge.tsx
+++ src/components/Knowledge/Knowledge.tsx
@@ -1,51 +1,10 @@
-import { useEffect, useRef } from 'react';
-import TagCloud from 'TagCloud';
+import { useRef } from 'react';
+import { TagCloud } from '@frank-mayer/react-tag-cloud';
 import { motion } from 'framer-motion';
 import './knowledge.css';
-const LangSphere = () => {
+const Knowledge = () => {
   const scrollRef = useRef(null);
-  const isMobile = window.innerWidth < 800;
-  useEffect(() => {
-    return () => {
-      const container: string = '.tagcloud';
-      const texts: string[] = [
-        'HTML',
-        'CSS',
-        'SASS',
-        'BEM',
-        'TailwindCSS',
-        'JavaScript',
-        'TypeScript',
-        'ReactJS',
-        'NodeJS',
-        'ES6',
-        'Python',
-        'Git',
-        'GitHub',
-        'Bash',
-        'Linux',
-        'Docker',
-        'Ansible',
-        'AWS',
-        'HTTP',
-        'DNS',
-        'API',
-        'JSON',
-      ];
-
-      const options: object = {
-        radius: !isMobile ? 300 : 150,
-        maxSpeed: 'fast',
-        initSpeed: 'normal',
-        keep: true,
-        containerClass: 'sphere',
-        itemClass: 'sphere--item',
-      };
-
-      TagCloud(container, texts, options);
-    };
-  }, []);
-
+  interface TagCloudOptions {}
   return (
     <motion.div
       initial={{ scale: 0 }}
@@ -54,11 +13,42 @@ const LangSphere = () => {
       viewport={{ root: scrollRef, once: true }}
       className="knowledge-sphere-box"
     >
-      <div className="knowledge-sphere">
-        <span className="tagcloud"></span>
-      </div>
+      <TagCloud
+        className="knowledge-sphere"
+        options={(w: Window & typeof globalThis): TagCloudOptions => ({
+          radius: Math.min(600, w.innerWidth, w.innerHeight) / 2,
+          maxSpeed: 'fast',
+          containerClass: 'sphere',
+          itemClass: 'sphere--item',
+        })}
+      >
+        {[
+          'HTML',
+          'CSS',
+          'SASS',
+          'BEM',
+          'TailwindCSS',
+          'JavaScript',
+          'TypeScript',
+          'ReactJS',
+          'NodeJS',
+          'ES6',
+          'Python',
+          'Git',
+          'GitHub',
+          'Bash',
+          'Linux',
+          'Docker',
+          'Ansible',
+          'AWS',
+          'HTTP',
+          'DNS',
+          'API',
+          'JSON',
+        ]}
+      </TagCloud>
     </motion.div>
   );
 };
 
-export default LangSphere;
+export default Knowledge;
