94b7b9df6ff996cfd62de3479139d24fa4e33184
rwxdan <dan1elcastro@proton.me>
2023-01-06 13:00:09 -0400

    Actually use autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
+});
