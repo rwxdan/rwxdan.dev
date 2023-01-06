95594a86d56374ae4c73999da09340f0d9bd520d
rwxdan <dan1elcastro@proton.me>
2023-01-06 15:23:15 -0400

    Create ContactLinks array

--- src/globals/constants/index.ts
+++ src/globals/constants/index.ts
@@ -1,3 +1,12 @@
+import {
+  Discord,
+  Telegram,
+  Protonmail,
+  Linkedin,
+  GitHub,
+  FrontendMentor,
+} from '../../assets/icons';
+
 export const navLinks: {
   title: string;
   id: string;
@@ -23,3 +32,40 @@ export const navLinks: {
 export const Mail = 'rwxdan@gmail.com';
 export const Gh = 'https://github.com/rwxdan';
 export const In = 'https://www.linkedin.com/in/rwxdan/';
+
+export const ContactLinks: {
+  id: string;
+  link: string;
+  icon: string;
+}[] = [
+  {
+    id: 'protonmail',
+    link: 'mailto:rwxdan@proton.me',
+    icon: Protonmail,
+  },
+  {
+    id: 'linkedin',
+    link: 'https://www.linkedin.com/in/rwxdan/',
+    icon: Linkedin,
+  },
+  {
+    id: 'github',
+    link: 'https://github.com/rwxdan',
+    icon: GitHub,
+  },
+  {
+    id: 'frontendmentor',
+    link: 'https://www.frontendmentor.io/profile/rwxdan',
+    icon: FrontendMentor,
+  },
+  {
+    id: 'discord',
+    link: 'https://discordapp.com/users/rwxdan#1884',
+    icon: Discord,
+  },
+  {
+    id: 'telegram',
+    link: 'https://t.me/dan1elcastro',
+    icon: Telegram,
+  },
+];
