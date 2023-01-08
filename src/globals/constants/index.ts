0bbd5bc94d22cd5d6c27200792830fc2868f2fbc
rwxdan <dan1elcastro@proton.me>
2023-01-08 00:09:59 -0400

    Expand index

--- src/globals/constants/index.ts
+++ src/globals/constants/index.ts
@@ -79,14 +79,16 @@ export const ContactLinks: {
 ];
 
 export const featured: {
+  id: string;
   name: string;
-  info: string;
+  info?: string;
   stack: string[];
-  code: string;
+  code?: string;
   live: string;
-  image: string;
+  image?: string;
 }[] = [
   {
+    id: 'blogr',
     name: 'Blogr Landing Page',
     info: 'A landing page for a fictional platform. The assets, design and guidelines were provided by Frontend Mentor.',
     stack: ['PostCSS', 'React', 'TailwindCSS', 'Vite'],
@@ -95,6 +97,7 @@ export const featured: {
     image: Blogr,
   },
   {
+    id: 'hoobank',
     name: 'Hoobank',
     info: "HooBank is a landing page whose gorgeous design was created by @adrianhajdin and completed by their community over at Figma. This build gave me a much-needed understanding of gradients and blur, and it's also one of my first projects using TailwindCSS.",
     stack: ['PostCSS', 'React', 'TailwindCSS', 'Vite'],
@@ -103,6 +106,7 @@ export const featured: {
     image: Hoobank,
   },
   {
+    id: 'manage',
     name: 'Manage Landing Page',
     info: "Manage is one of the most fun builds I've coded so far. This SPA has a relatively complex layout and its responsiveness was a real challenge. The assets, design and guidelines were provided by Frontend Mentor.",
     stack: ['PostCSS', 'React', 'Framer Motion', 'Vite'],
@@ -111,11 +115,47 @@ export const featured: {
     image: Manage,
   },
   {
+    id: 'tip',
     name: 'Tip Calculator App',
     info: "Supose that you're on a restaurant with some friends and you want to calculate the amount of money by tip percentage that each one has to pay, plus the bill amount. Tip Calculator lets you easily do that! The assets, design and guidelines were provided by Frontend Mentor.",
     stack: ['HTML', 'JavaScript', 'SCSS'],
     code: 'https://github.com/rwxdan/tip-calculator-app',
     live: 'https://rwxdan.github.io/tip-calculator-app',
     image: TipCalculator,
+  },
+  {
+    id: 'adviceGenerator',
+    name: 'Advice Generator App',
+    stack: ['HTML', 'SCSS', 'JavaScript', 'API'],
+    code: 'https://github.com/rwxdan/advice-generator-app',
+    live: 'https://rwxdan.github.io/advice-generator-app',
+  },
+  {
+    id: 'huddleExtended',
+    name: 'Huddle Landing Page Extended',
+    stack: ['HTML', 'SCSS'],
+    code: 'https://github.com/rwxdan/huddle-landing-page-extended',
+    live: 'https://rwxdan.github.io/huddle-landing-page-extended/',
+  },
+  {
+    id: 'dragonRepeller',
+    name: 'Dragon Repeller',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    code: 'https://github.com/rwxdan/dragon-repeller-minigame',
+    live: 'https://rwxdan.github.io/dragon-repeller-minigame',
+  },
+  {
+    id: 'orderSummary',
+    name: 'Order Summary Component',
+    stack: ['HTML', 'SCSS'],
+    code: 'https://github.com/rwxdan/order-summary-component',
+    live: 'https://rwxdan.github.io/order-summary-component',
+  },
+  {
+    id: 'newsHomepage',
+    name: 'News Homepage',
+    stack: ['React', 'SCSS', 'Vite'],
+    code: 'https://github.com/rwxdan/news-homepage',
+    live: 'https://news-homepage-nu-ashy.vercel.app/',
   },
 ];
