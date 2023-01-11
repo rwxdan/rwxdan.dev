4559fba165749b9c2430df4e07cc9ec7e1ed8515
rwxdan <dan1elcastro@proton.me>
2023-01-11 15:19:41 -0400

    Don't check isMobile
    This component is not rendered on smaller screens anyway. Thanks Safari.

--- src/components/Knowledge/Knowledge.tsx
+++ src/components/Knowledge/Knowledge.tsx
@@ -2,14 +2,12 @@ import { TagCloud } from '@frank-mayer/react-tag-cloud';
 import './knowledge.css';
 const Knowledge = () => {
   interface TagCloudOptions {}
-  const isMobile = window.innerWidth < 470;
   return (
     <div className="knowledge-sphere-box">
       <TagCloud
         className="knowledge-sphere"
         options={(w: Window & typeof globalThis): TagCloudOptions => ({
-          radius:
-            Math.min(!isMobile ? 600 : 440, w.innerWidth, w.innerHeight) / 2,
+          radius: Math.min(600, w.innerWidth, w.innerHeight) / 2,
           maxSpeed: 'fast',
           containerClass: 'sphere',
           itemClass: 'sphere--item',
