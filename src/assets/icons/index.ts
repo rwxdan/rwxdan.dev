ac18d9fd970c9c18cb049d6bc5d8d8280b1b84f5
rwxdan <dan1elcastro@proton.me>
2023-01-14 19:20:25 -0400

    Use react-icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,10 +1,2 @@
 export { default as Menu } from './menu.svg';
 export { default as Close } from './close.svg';
-export { default as Share } from './share.svg';
-export { default as Discord } from './discord.svg';
-export { default as FrontendMentor } from './frontendmentor.svg';
-export { default as GitHub } from './github.svg';
-export { default as Gmail } from './gmail.svg';
-export { default as Linkedin } from './linkedin.svg';
-export { default as Protonmail } from './protonmail.svg';
-export { default as Telegram } from './telegram.svg';
