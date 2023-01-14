55bc658cd6b99e3f41fe5e36b6c51c400cd9b11b
rwxdan <dan1elcastro@proton.me>
2023-01-14 19:28:21 -0400

    Receive info property
    - Use react-icons

--- src/components/MoreProjects/MoreProjects.tsx
+++ src/components/MoreProjects/MoreProjects.tsx
@@ -1,6 +1,8 @@
 import { animated, useSpring } from '@react-spring/web';
 import { featured } from '../../constants';
-import { GitHub, Share } from '../../assets/icons';
+import { SiGithub } from 'react-icons/si';
+import { FiShare } from 'react-icons/fi';
+import { BsFillBookmarkStarFill } from 'react-icons/bs';
 const MoreProjects = () => {
   const box = useSpring({
     from: {
@@ -29,32 +31,24 @@ const MoreProjects = () => {
     <animated.ul style={box} className="more">
       {featured.slice(4).map((project) => (
         <animated.li style={item} key={project.id}>
-          <a
-            target="_blank"
-            href={project.live || project.code}
-            className="card"
-          >
+          <a target="_blank" href={project.live || project.code} className="card">
+            <div className="decoration">
+              <BsFillBookmarkStarFill />
+            </div>
             <div className="relevant">
               <h6 className="name">{project.name}</h6>
               <div className="external-links-box">
-                <a
-                  target="_blank"
-                  href={project.code}
-                  className="external-link"
-                >
-                  <img src={GitHub} alt="GitHub repo" className="icon" />
+                <a target="_blank" href={project.code} className="external-link">
+                  <SiGithub />
                 </a>
                 {project.live && (
-                  <a
-                    target="_blank"
-                    href={project.live}
-                    className="external-link"
-                  >
-                    <img src={Share} alt="Live preview" className="icon" />
+                  <a target="_blank" href={project.live} className="external-link">
+                    <FiShare />
                   </a>
                 )}
               </div>
             </div>
+            <p className="info">{project.info}</p>
             <ul className="stack">
               {project.stack.map((item, index) => (
                 <li key={index} className="item">
