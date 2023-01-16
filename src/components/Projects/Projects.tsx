0e7bb78f144a94e2eab95f2afc7d79ba92d4b02c
rwxdan <dan1elcastro@proton.me>
2023-01-15 20:15:21 -0400

    Fix
    - Animate divs, not the whole section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -23,24 +23,26 @@ const Projects = () => {
       },
     ));
   return (
-    <animated.section style={springs} ref={ref} id="projects" className="projects">
-      <h3 className="title">Featured projects</h3>
-      <h4 className="sub-title">Take a look at some of my builds</h4>
-      <div className="projects-box">
-        {featured.slice(0, 4).map((project) => (
-          <Sample
-            key={project.id}
-            name={project.name}
-            info={project.info}
-            stack={project.stack}
-            code={project.code}
-            live={project.live}
-            image={project.image}
-          />
-        ))}
-      </div>
-      <ShowMore />
-    </animated.section>
+    <section ref={ref} id="projects">
+      <animated.div style={springs} className="projects">
+        <h3 className="title">Featured projects</h3>
+        <h4 className="sub-title">Take a look at some of my builds</h4>
+        <div className="projects-box">
+          {featured.slice(0, 4).map((project) => (
+            <Sample
+              key={project.id}
+              name={project.name}
+              info={project.info}
+              stack={project.stack}
+              code={project.code}
+              live={project.live}
+              image={project.image}
+            />
+          ))}
+        </div>
+        <ShowMore />
+      </animated.div>
+    </section>
   );
 };
 
