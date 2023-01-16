3cde2c3b53d94e5e151fa9f296a70983e4912608
rwxdan <dan1elcastro@proton.me>
2023-01-16 17:08:36 -0400

    New
    - Make my life easier with path aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [autoprefixer({})],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@static': path.resolve(__dirname, './static'),
     },
   },
 });
