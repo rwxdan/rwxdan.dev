c7cf2c9e5955f00b4b2e2cf0a7f686786b9b0d52
rwxdan <dan1elcastro@proton.me>
2023-01-17 01:11:26 -0400

    New
    - navLinks.ts

---
+++ src/shared/navLinks.ts
@@ -0,0 +1,23 @@
+const navLinks: {
+  title: string;
+  id: string;
+}[] = [
+  {
+    title: 'Projects',
+    id: 'projects',
+  },
+  {
+    title: 'Connect',
+    id: 'connect',
+  },
+  {
+    title: 'About',
+    id: 'about',
+  },
+  {
+    title: 'Resume',
+    id: 'resume',
+  },
+];
+
+export { navLinks };
