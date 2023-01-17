e08ba6ddee7b0aa2461b92239ae841e7ac395bf2
rwxdan <dan1elcastro@proton.me>
2023-01-17 01:11:54 -0400

    New
    - connectLinks.ts

---
+++ src/shared/connectLinks.ts
@@ -0,0 +1,39 @@
+const Mail = 'rwxdan@gmail.com';
+const Gh = 'https://github.com/rwxdan';
+const In = 'https://www.linkedin.com/in/rwxdan/';
+
+const connectLinks: {
+  id: string;
+  link: string;
+}[] = [
+  {
+    id: 'gmail',
+    link: 'mailto:rwxdan@gmail.com',
+  },
+  {
+    id: 'protonmail',
+    link: 'mailto:rwxdan@proton.me',
+  },
+  {
+    id: 'linkedin',
+    link: 'https://www.linkedin.com/in/rwxdan/',
+  },
+  {
+    id: 'github',
+    link: 'https://github.com/rwxdan',
+  },
+  {
+    id: 'frontendmentor',
+    link: 'https://www.frontendmentor.io/profile/rwxdan',
+  },
+  {
+    id: 'discord',
+    link: 'https://discordapp.com/users/rwxdan#1884',
+  },
+  {
+    id: 'telegram',
+    link: 'https://t.me/dan1elcastro',
+  },
+];
+
+export { Mail, Gh, In, connectLinks };
