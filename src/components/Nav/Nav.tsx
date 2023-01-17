e5150ecb00161353c68b37db7020b34eff05f164
rwxdan <dan1elcastro@proton.me>
2023-01-17 01:14:29 -0400

    Org
    - Import navLinks from "@/shared"

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { animated, useSpring } from '@react-spring/web';
-import './nav.css';
 import logo from '@static/logo.svg';
 import { Menu, Close } from '@static/icons';
-import { navLinks } from '@/constants';
+import { navLinks } from '@/shared';
+import './nav.css';
 const Nav = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
 
