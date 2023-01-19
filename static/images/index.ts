93672e448075396060ad18287d5a3c21bf5fa6d6
rwxdan <dan1elcastro@proton.me>
2023-01-19 14:43:38 -0400

    Rev
    - Embrace png

--- static/images/index.ts
+++ static/images/index.ts
@@ -1,5 +1,5 @@
 export { default as Me } from './me.png';
-export { default as EvoGym } from './evogym.svg';
+export { default as EvoGym } from './evogym.png';
 export { default as Hoobank } from './hoobank.svg';
 export { default as Manage } from './manage.svg';
 export { default as TipCalculator } from './tip_calculator.svg';
