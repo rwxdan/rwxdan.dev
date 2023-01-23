39b543149d5e73cbab630f8474e068fe19e3c2a7
rwxdan <dan1elcastro@proton.me>
2023-01-23 00:22:22 -0400

    Rev
    - Friendship ended with png & svg, webp
    
    Look at those compression rates! 🤯

--- static/images/index.ts
+++ static/images/index.ts
@@ -1,5 +1,5 @@
-export { default as Me } from './me.png';
-export { default as EvoGym } from './evogym.png';
-export { default as Hoobank } from './hoobank.svg';
-export { default as Manage } from './manage.svg';
-export { default as TipCalculator } from './tip_calculator.svg';
+export { default as Me } from './me.webp';
+export { default as EvoGym } from './evogym.webp';
+export { default as Hoobank } from './hoobank.webp';
+export { default as Manage } from './manage.webp';
+export { default as TipCalculator } from './tip_calculator.webp';
