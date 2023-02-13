2de8508baf5e131ad855b969ff413abbd1121bcc
rwxdan <rwxdan@gmail.com>
2023-02-13 16:21:37 -0400

    Update index.ts
    - Delete unused hoobank.webp

--- static/images/index.ts
+++ static/images/index.ts
@@ -1,6 +1,5 @@
 export { default as Me } from './me.webp';
 export { default as EvoGym } from './evogym.webp';
-export { default as Hoobank } from './hoobank.webp';
 export { default as Manage } from './manage.webp';
 export { default as TipCalculator } from './tip_calculator.webp';
 export { default as darFi } from './dar-fi.webp';
