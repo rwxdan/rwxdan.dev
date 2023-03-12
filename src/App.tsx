a9fc51911e8fce9dc9221c3eb2a836d6b09a4866
rwxdan <rwxdan@gmail.com>
2023-03-12 13:10:46 -0400

    Clean up
    - Remove `react-helmet` dependency: this project doesn't need
      such abstraction layer.
    - Use proper values inside the website's `<head>`: wtf I was
      thinking.
    
    Now the page achieves 100 lighthouse scores... Except for performance.
    I'm giving that one up on purpose.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { LoadingEffect, Nav, AnchorLinks, AnchorMail, Hero, WhoAmI, Projects, Connect, Footer } from '@/components';
 const App = () => {
   const [Loading, setLoading] = useState(true);
@@ -9,17 +8,7 @@ const App = () => {
   }, []);
 
   return (
-    <HelmetProvider>
-      <Helmet>
-        <meta charSet="UTF-8" />
-        <meta name="title" content="Daniel Castro" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Spline+Sans+Mono:wght@400;500;700&family=Inter:wght@400;500;700&family=Work+Sans:wght@600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
+    <>
       {Loading ? (
         <LoadingEffect />
       ) : (
@@ -36,7 +25,7 @@ const App = () => {
           <Footer />
         </div>
       )}
-    </HelmetProvider>
+    </>
   );
 };
 
