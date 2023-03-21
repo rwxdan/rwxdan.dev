62d62df12ac91cf042659e85310d937364dfa78c
rwxdan <rwxdan@gmail.com>
2023-03-21 00:14:23 -0400

    Delete `LoadingEffect` component
    Reason: It was cool but wrongly implemented.
    
    It also, makes the whole page feels slow and
    heavy-sized when it really isn't. (~1.04MB).
    
    The current entrance animations would do just
    fine.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import { useState, useEffect } from 'react';
-import { LoadingEffect, Nav, AnchorLinks, AnchorMail, Hero, WhoAmI, Projects, Connect, Footer } from '@/components';
+import { Nav, AnchorLinks, AnchorMail, Hero, WhoAmI, Projects, Connect, Footer } from '@/components';
 const App = () => {
-  const [Loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
-  }, []);
-
   return (
     <>
-      {Loading ? (
-        <LoadingEffect />
-      ) : (
-        <div>
-          <Nav />
-          <main>
-            <Hero />
-            <Projects />
-            <Connect />
-            <WhoAmI />
-            <AnchorLinks />
-            <AnchorMail />
-          </main>
-          <Footer />
-        </div>
-      )}
+      <Nav />
+      <main>
+        <Hero />
+        <Projects />
+        <Connect />
+        <WhoAmI />
+        <AnchorLinks />
+        <AnchorMail />
+      </main>
+      <Footer />
     </>
   );
 };
